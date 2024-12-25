4f6d85e93157d162833173904d144b4c792a8829
Gabriel Dipse <gabrieldipse@Gabriels-MacBook-Air.local>
2024-12-25 11:15:15 +0200

    Modificari minori-Proiectul funtioneaza

--- PetMedRegistry/settings.py
+++ PetMedRegistry/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
